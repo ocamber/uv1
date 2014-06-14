29422dfb48399e8cc22f0045301fc0cc5fa9ff67
Oren Camber <ocamber@gmail.com>
2014-06-13 22:59:25 -0400

    Reassign pins

--- gpio_pins.h
+++ gpio_pins.h
@@ -6,12 +6,12 @@
 */
 
 #define LIGHTS_GPIO             27
-#define LEFT_MOTOR_REV_GPIO     25
-#define LEFT_MOTOR_FWD_GPIO     24
-#define RIGHT_MOTOR_REV_GPIO    23
-#define RIGHT_MOTOR_FWD_GPIO    22
-#define SOUND_GPIO              17
-#define RANGE_ECHO_GPIO         15
-#define RANGE_TRIGGER_GPIO      14
+#define RIGHT_MOTOR_FWD_GPIO    25
+#define LEFT_MOTOR_FWD_GPIO     23
+#define RIGHT_MOTOR_REV_GPIO    24
+#define LEFT_MOTOR_REV_GPIO     22
 #define OBSTACLE_GPIO           8
 #define TOUCH_GPIO              7
+#define RANGE_ECHO_GPIO         4
+#define RANGE_TRIGGER_GPIO      3
+#define SOUND_GPIO              2
