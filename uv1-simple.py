897862bd323400da76063d6e6022d33daeca2ac7
Oren Camber <ocamber@gmail.com>
2014-07-23 20:14:11 -0400

    refactor movement code

--- uv1-simple.py
+++ uv1-simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # uv1-simple.py
-# Automated simple main procedure for RPI-UV1 drone.
+# Simple automated exploration procedure for RPI-UV1 drone.
 #
 # Author : Oren Camber
 # Date   : 2014-07-11
@@ -21,13 +21,14 @@ PHOTO_CMD = 'raspistill'
 VIDEO_CMD = 'raspivid'
 LIGHTS_GPIO = 14
 LASER_GPIO  = 7
-MOTOR_CORRECTION = "FC"
-CORRECTION_RATIO = 0.05
-CORRECTION_INTERVAL = 500
-MOTOR_DEG = 5.83 
-MOTOR_CM = 52.5
+FWD_MOTOR_CORRECTION = "FC"
+RVS_MOTOR_CORRECTION = "CF"
+MOTOR_CORRECTION_RATIO = 0.05
+MAX_MOTOR_INTERVAL_CM = 10
+MOTOR_MS_PER_DEG = 5.83
+MOTOR_MS_PER_CM = 52.5
 PROXIMITY_SENSORS = "TO"
-PARTIAL_TURN = "FR210"
+PARTIAL_TURN = "FR190"
 BACK_AWAY = "RR200"
 
 
@@ -40,105 +41,121 @@ GPIO.output(LIGHTS_GPIO, False)
 GPIO.output(LASER_GPIO, False)
 
 
-def main(): 
+def main():
 
-    sensor_signals = None
-    
-    # Initialize randomizer
+    # Initialize randomizer and sensor readings
     random.seed()
-    
+    sensor_signals = None
+
     while True:
-        
+
         # React to sensor input
         sensor_signals = read_sensors()
         if sensor_signals['sound']:
             break
         if sensor_signals['obstacle'] or sensor_signals['touch']:
-            back_away_from_obstacle()            
+            back_away_from_obstacle()
+            rotate_to_avoid_obstacle()
+            continue
+        if sensor_signals['range'] < 10:
+            rotate_to_avoid_obstacle()
             continue
             
-        # Move random direction and distance (if range < 10 cm turn a lot)
-        if sensor_signals['range'] < 10:
-            rotation = random.randint(90,180)
-        else:
-            rotation = random.randint(30,230)            
-        distance = random.randint(50,300)
-        movement_result = proceed(rotation, distance)
-        data = survey_surroundings()
+        # Periodically scan surroundings
+        if random.randint(0,10)<1:
+            survey_surroundings()
 
+        # Periodically rotate to new direction
+        if random.randint(0,10)<3:
+            rotate(random.randint(-60,60))
+            continue
 
-def proceed(rotation, distance):
-    # convert degrees and cm to motor control values
-    rot_value = int(rotation * MOTOR_DEG + 0.5)
-    dist_value = int(distance * MOTOR_CM + 0.5)
-    sensor_signals = read_sensors()
-    if sensor_signals['sound'] or sensor_signals['obstacle'] \
-    or sensor_signals['touch'] or sensor_signals['range'] < 10:
+        # Go forward random cm to a new position
+        go_forward(random.randint(5,MAX_MOTOR_INTERVAL_CM))
+
+def rotate(degrees):
+    if not degrees:
         return
-    try:
-        movement_result = subprocess.call([MOTORS_CMD, "FR"+str(rot_value)])
-    except:
-        pass    
-    remainder = dist_value
-    while remainder > 0:
-        sensor_signals = read_sensors()
-        if sensor_signals['sound'] or sensor_signals['obstacle'] \
-        or sensor_signals['touch'] or sensor_signals['range'] < 10:
-            return
-        interval = remainder
-        if interval > CORRECTION_INTERVAL:
-            interval = CORRECTION_INTERVAL
-        try:
-            movement_result = subprocess.call([MOTORS_CMD, "FF"+str(interval)])
-        except:
-            pass
-        remainder = remainder - interval;
-        correction = int(CORRECTION_RATIO * interval)
-        try:
-            movement_result = subprocess.call([MOTORS_CMD, MOTOR_CORRECTION+str(correction)])
-        except:
-            pass
-        
-    
+
+    # convert degrees to motors / milliseconds
+    ms = int(abs(degrees) * MOTOR_MS_PER_DEG + 0.5)
+    motors = "FR"
+    if degrees < 0:
+        motors = "RF"
+    movement_result = subprocess.call([MOTORS_CMD, motors+str(ms)])
+    log_motion(movement_result)
+    return movement_result
+
+def go_forward(cm):
+    if not cm:
+        return
+
+    # convert cm to motors / milliseconds
+    ms = int(abs(cm) * MOTOR_MS_PER_CM + 0.5)
+    correction = "FC"
+    motors = "FF"
+    if cm < 0:
+        motors = "RR"
+        movement_result = subprocess.call([MOTORS_CMD, motors+str(ms)])
+        motors = "CF"
+        subprocess.call([MOTORS_CMD, motors+str(ms)])
+    else
+        motors = "FF"
+        movement_result = subprocess.call([MOTORS_CMD, motors+str(ms)])
+        motors = "FC"
+        subprocess.call([MOTORS_CMD, motors+str(ms)])
+
+    # Execute turn to correct straightness
+    subprocess.call([MOTORS_CMD, motors+str(int(CORRECTION_RATIO * ms))])
+    log_motion(movement_result)
+    return movement_result
+
 def back_away_from_obstacle():
-    try:
-        subprocess.call([RESET_SENSORS_CMD, PROXIMITY_SENSORS])
-        subprocess.call([MOTORS_CMD, BACK_AWAY])
-    except:
-        pass
-        
+    subprocess.call([RESET_SENSORS_CMD, PROXIMITY_SENSORS])
+    movement_result = subprocess.call([MOTORS_CMD, BACK_AWAY])
+    subprocess.call([RESET_SENSORS_CMD, PROXIMITY_SENSORS])
+    log_motion(movement_result)
+    return movement_result
+
+def rotate_to_avoid_obstacle():
+    subprocess.call([RESET_SENSORS_CMD, PROXIMITY_SENSORS])
+    movement_result = rotate(random.randint(90,180))
+    subprocess.call([RESET_SENSORS_CMD, PROXIMITY_SENSORS])
+    log_motion(movement_result)
+    return movement_result
 
 def read_sensors():
-    results = { 'touch':0, 'obstacle':0, 'sound':0, 'range':0 }
+    reading = { 'touch':0, 'obstacle':0, 'sound':0, 'range':0 }
     with open(SENSOR_FILE, 'r') as file:
         file_text = file.read()
     if file_text[0:1]=='T' and file_text[1:2]=='+':
-        results['touch'] = 1
+        reading['touch'] = 1
     if file_text[2:3]=='O' and file_text[3:4]=='+':
-        results['obstacle'] = 1
+        reading['obstacle'] = 1
     if file_text[4:5]=='S' and file_text[5:6]=='+':
-        results['sound'] = 1
+        reading['sound'] = 1
     if file_text[6:7]=='R':
-        results['range'] = int(file_text[7:10], base=10)
-    return results
-    
+        reading['range'] = int(file_text[7:10], base=10)
+    return reading
 
 def survey_surroundings():
-    results = []
-    img_file_prefix = datetime.datetime.now().strftime(IMG_FILE)
+    survey = []
+    img_group = datetime.datetime.now().strftime(IMG_FILE)
     for i in range(0, 10):
         sensor_signals = read_sensors()
-        if sensor_signals['sound'] or sensor_signals['obstacle'] or sensor_signals['touch']:
-            return results        
-        img_file = img_file_prefix + str(i) + '.jpg'
-        try:
-            subprocess.call([MOTORS_CMD, PARTIAL_TURN])
-            subprocess.call([PHOTO_CMD, "-n", "-o", img_file])
-        except:
-            pass
-        sensor_signals = read_sensors()
-        results.append({ 'sensors':sensor_signals, 'image':img_file })
-    return results
+        img_file = img_group + str(i) + '.jpg'
+        subprocess.call([PHOTO_CMD, "-n", "-o", img_file])
+        survey.append({ 'sensors':sensor_signals, 'image':img_file })
+        if sensor_signals['sound'] or sensor_signals['touch']:
+            break
+        subprocess.call([MOTORS_CMD, PARTIAL_TURN])
+    log_survey(survey)
+    return survey
 
+def log_survey():
+    pass
 
+def log_motion():
+    pass
+    
 main()
